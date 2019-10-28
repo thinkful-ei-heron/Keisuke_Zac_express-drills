bab8066f93f745eaceffc7dcd1c9f10456e04e96
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-10-28 12:08:21 -0500

    working on cipher

--- app.js
+++ app.js
@@ -24,3 +24,36 @@ app.get('/sum', (req, res) => {
 	res.send(`The sum of ${a} and ${b} is ${sum}`);
 });
 
+app.get('/cipher', (req, res) => {
+	const text = req.query.text;
+	const shift = req.query.shift;
+
+	if (!parseInt(shift)) {
+		return res.status(400).send('Shift must be an integer');
+	}
+
+	let result = '';
+	for (let i = 0; i < text.length; i++) {
+		const input = text.charCodeAt(i);
+		let alpha;
+
+		if (input <= 90) {
+			alpha = 'upper';
+		}
+		else if (input >= 97) {
+			alpha = 'lower';
+		}
+
+		let code = (text.charCodeAt(i) + shift);
+		if (code > 90 && alpha === 'upper') {
+			code -= 26;
+		}
+		else if (code > 122 && alpha === 'lower') {
+			code -= 26;
+		}
+
+		result += String.fromCharCode(code);
+	};
+
+	res.send(result);
+})
